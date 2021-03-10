db27f81cd984a6717e1ef2f03d85c7e9505d6d6c
lispalding <lisette.spalding@gmail.com>
2021-03-10 14:07:50 -0700

    03/10/2021 Push -- 2:07PM

--- main.py
+++ main.py
@@ -19,15 +19,13 @@ title = "Shmup"
 
 WIDTH = 300
 HEIGHT = 600
-
-FPS = 60
 ############## !! FIN !! ###############
 
 ############# !! CLASSES !! ############
 class Player(pg.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__() # Setting the __init__ function as the super class
-        self.image = pg.Surface((50, 40)) # Setting the height and width of the player sprite
+        self.image = pg.Surface((20, 40)) # Setting the height and width of the player sprite
         self.image.fill(GREEN) # Filling the sprite image a certain color
 
         self.rect = self.image.get_rect()
@@ -36,25 +34,109 @@ class Player(pg.sprite.Sprite):
 
         self.speedx = 0
 
+        self.keypressed = False
+
+    def shoot(self):
+        b = Bullet(self.rect.centerx, self.rect.top-1)
+        allSprites.add(b)
+        bulletGroup.add(b)
+
+    def togglePressed(self):
+        """ To use: self.togglePressed()
+         This is the function that will check to see if a keypress is False. """
+        self.keypressed = False
+        self.speedx = 0
+
     def update(self):
+        """ To use: self.update()
+         This is the function that will update the movement of the player character. """
+        keystate = pg.key.get_pressed() # Checking the Keystate, whether it is pressed or not
+
+        ########## !!!! .. FLOW MOVEMENT .. !!!! ##########
+        if self.keypressed == False:
+            if keystate[pg.K_LEFT] or keystate[pg.K_a]:
+                self.speedx += -3
+                self.keypressed = True
+
+            if keystate[pg.K_RIGHT] or keystate[pg.K_d]:
+                self.speedx += 3
+                self.keypressed = True
+
         self.rect.x += self.speedx
+        ########## !!!! .. FLOW FINISHED .. !!!! ##########
+
+        # if keystate[K_SPACE]:
+        #     self.shoot()
+
+        ######### !!!! .. SCREEN BINDING .. !!!! ##########
+        ## Binding player to screen area...
+        if self.rect.left <= 0:
+            self.rect.left = 0
+
+        if self.rect.top <= 0:
+            self.rect.top = 0
+
+        if self.rect.bottom >= HEIGHT:
+            self.rect.bottom = HEIGHT
+
+        if self.rect.right >= WIDTH:
+            self.rect.right = WIDTH
+        ######### !!!! .. BINDING FINISH .. !!!! ##########
+
+class Bullet(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Bullet, self).__init__()
+
+        ## Creating the bullet image
+        self.image = pg.Surface((10, 20))  # Setting the height and width of the player sprite
+        self.image.fill(BLUE)  # Filling the sprite image a certain color
+
+        # Positioning the bullet
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+
+        self.speedy = -5
+
+    def update(self):
+        self.rect.y += self.speedy
+
+        if self.rect.bottom < 0:
+            self.kill()
 
 class NPC(pg.sprite.Sprite):
     def __init__(self):
         super(NPC, self).__init__()  # Setting the __init__ function as the super class
-        self.image = pg.Surface((50, 40))  # Setting the height and width of the player sprite
+        self.image = pg.Surface((20, 20))  # Setting the height and width of the player sprite
         self.image.fill(RED)  # Filling the sprite image a certain color
 
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = (HEIGHT * .08)
 
-        self.speedx = 0
-        self.speedy = 0
+        self.randSpeedX = r.randrange(2,4)
+        self.randSpeedY = r.randrange(2,4)
+
+        self.speedx = self.randSpeedX
+        self.speedy = self.randSpeedY
 
     def update(self):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
+
+        ##### !! SCREEEN WRAPPING !! #####
+        if self.rect.left > WIDTH:
+            self.rect.left = 0
+
+        if self.rect.right < 0:
+            self.rect.right = WIDTH
+
+        if self.rect.top > HEIGHT:
+            self.rect.top = 0
+
+        if self.rect.bottom < 0:
+            self.rect.bottom = HEIGHT
+        ##### !! WRAPPING FINISH !! #####
 
 ############## !! FIN !! ###############
 
@@ -64,26 +146,33 @@ pg.mixer.init()
 
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption(title)
-clock = pg.time.Clock
+clock = pg.time.Clock()
 ############## !! FIN !! ###############
 
 ########## !! GAME OBJECTS !! ###########
+######### .. SPRITES .. ##########
 player = Player()
-npc = NPC()
-############## !! FIN !! ###############
 
-######### !! SPRITE GROUPS !! ##########
 allSprites = pg.sprite.Group()
 playersGroup = pg.sprite.Group()
 npcGroup = pg.sprite.Group()
+bulletGroup = pg.sprite.Group()
 
 # Adding Player and NPC to sprite groups
 playersGroup.add(player)
-npcGroup.add(npc)
+
+for i in range(12):
+    npc = NPC()
+    npcGroup.add(npc)
+
+bullet = Bullet(WIDTH/2, HEIGHT/2)
+bulletGroup.add(bullet)
+allSprites.add(bullet)
 
 # Auto adding sprites to allSprites group
 for i in playersGroup:
     allSprites.add(i)
+
 for i in npcGroup:
     allSprites.add(i)
 ############## !! FIN !! ###############
@@ -98,23 +187,66 @@ playing = True
 score = 0
 level = 1
 difficulty = 0
+
+FPS = 60
 ########## .. FIN .. ###########
+
+######### !! GAME FUNCTIONS !! #########
+def spawnNpc():
+    npc = NPC()
+    npcGroup.add(npc)
+    allSprites.add(npc)
+
+######### !! GAME FUNC FINISH !! #######
 
 ######## .. GAME LOOP .. ########
 while playing:
     ### Timing
-
+    clock.tick(FPS)  # Controlling the loop
 
     ### Collecting Input
-    ## Quitting the game when we hit "x" or hits escape
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
+    ##### Processing input ######
+    for event in pg.event.get():  # Getting a list of events that have happened
+        if event.type == pg.KEYUP: ## Getting if a key has been released
+            if (event.key == pg.K_LEFT) or (event.key == pg.K_a):
+                player.togglePressed()
+
+            if (event.key == pg.K_RIGHT) or (event.key == pg.K_d):
+                player.togglePressed()
+
+            if (event.key == pg.K_UP) or (event.key == pg.K_w):
+                player.togglePressed()
+
+            if (event.key == pg.K_DOWN) or (event.key == pg.K_s):
+                player.togglePressed()
+
+        ### TEMPORARY FIX FOR BULLETS ###
+        if event.type == KEYDOWN:
+            if event.key == pg.K_SPACE:
+                player.shoot()
+        ####### FIN TEMPORARY FIX #######
+
+        if event.type == pg.QUIT: ## Quitting the game when we hit "x" or hits escape
             playing = False
         if event.type == pg.KEYDOWN:
             if event.key == K_ESCAPE:
                 playing = False
 
     ### Updating Section
+    ## If NPC hits Player
+    hits0 = pg.sprite.spritecollide(player, npcGroup, True)
+
+    if hits0: # Spawning an NPC when player gets hit by NPC
+        spawnNpc()
+    #     playing = False
+
+    ## If Bullet hits NPC
+    hits1 = pg.sprite.groupcollide(npcGroup, bulletGroup, True, True)
+
+    if hits1: # Spawning an NPC when an asteroid hits bullet
+        spawnNpc()
+
+    allSprites.update()
 
     ### Display Changes / Render
     screen.fill(BLACK)
